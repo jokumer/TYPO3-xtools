ff40b384b8dbd5f560df22ef52e68127bf43d255
JKummer <typo3@enobe.de>
2017-02-21 15:54:22 +0100

    [TASK] add pagination to backend lists

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -16,6 +16,13 @@ use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
 class AbstractController extends ActionController
 {
     /**
+     * Extension configuration
+     * 
+     * @var array
+     */
+    protected $extensionConfiguration = [];
+
+    /**
      * Initialize view
      *
      * @param ViewInterface $view
@@ -24,8 +31,21 @@ class AbstractController extends ActionController
     protected function initializeView(ViewInterface $view)
     {
         parent::initializeView($view);
-        // Add controller/action name for layout
-        $view->assign('controllerName',$this->request->getControllerName());
-        $view->assign('actionName',$this->request->getControllerActionName());
+        // Assign controller/action name for layout
+        $view->assign('controllerName', $this->request->getControllerName());
+        $view->assign('actionName', $this->request->getControllerActionName());
+        // Assign extension configuration
+        $this->initializeExtensionConfiguration();
+        $view->assign('extensionConfiguration', $this->extensionConfiguration);
+    }
+
+    /**
+     * Initialize extension configuration
+     *
+     * @return void
+     */
+    protected function initializeExtensionConfiguration()
+    {
+        $this->extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['xtools']);
     }
 }
